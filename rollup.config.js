8b7fd541dab844cf94d1fb8792456f8dd3e9292b
paulwilcox <t78t78@gmail.com>
2020-03-08 17:15:17 -0700

    Further package initializations.

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import license from 'rollup-plugin-license';
+import fs from 'fs';
+
+// Run license() here, not in the objects of the exported 
+// array.  Otherwise, the third party licence file gets 
+// overwritten, not appended to.
+let licensePlugin = license({
+    banner: { content: { file: 'license.md' } },
+    thirdParty: {
+        output: 'license-3rd-party',
+        includePrivate: true
+    }
+});
+
+let printCss = fs.readFileSync('./src/print.css');
+fs.writeFileSync(
+    './src/print.css.js', 
+    `export default \`\n\n${printCss}\n\n\``
+)
+
+export default [{
+    input: 'src/face.js',
+    output: {
+        file: 'dist/face.js',
+        format: 'cjs'
+    },
+    plugins: licensePlugin
+}];
+
